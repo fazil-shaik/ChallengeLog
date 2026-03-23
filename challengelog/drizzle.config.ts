1307e4f56e1b261a87efcf699f06ebee8746064a
Fazil-shaik <ammia4770@gmail.com>
2026-03-23 10:36:27 +0530

    Added changes for drizzle-kit. config things

--- challengelog/drizzle.config.ts
+++ challengelog/drizzle.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from "drizzle-kit";
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Source - https://stackoverflow.com/a/79502172
+// Posted by yffaffy
+// Retrieved 2026-03-23, License - CC BY-SA 4.0
+
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./app/(Schema)",
-  out: "./drizzle",
+    schema: './app/(Schema)',
 
-    driver: "pglite",
     dbCredentials: {
-    url: './db',
-  },
-});
+        url: process.env.DATABASE_URL!
+    },
 
+    verbose: true,
+    strict: true,
+    dialect: 'postgresql'
+});
